6a6e484a285f59c74476d69cb4fb275ecb4b8818
S <guerreqq@gmail.com>
2023-01-27 16:16:15 +0800

    clac avg length of reviews

--- read.py
+++ read.py
@@ -6,5 +6,10 @@ with open('reviews.txt', 'r') as f:
 	 	count += 1 # = count = count + 1
 	 	if count % 1000 == 0: # '%'=求餘數;=>如果count 對1000的餘數是0
 	 		print (len(date))
-print(len(date))
-print(date[0])
+print('檔案讀取完畢,總共有' ,len(date), '筆資料')
+
+
+sum_len = 0
+for d in date:
+	sum_len += len(d) #sum_len = sum_len + len(d)
+print('留言平均長度是', sum_len/len(date),'個字')
